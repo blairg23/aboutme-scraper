d54622a1daf12d55d8e271e272f2a300b14408e1
blairg23 <blairg23@gmail.com>
2016-01-07 19:10:35 +0000

    Added mysql dependency to install script

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,3 +1,3 @@
-sudo apt-get install libxml2-dev libxslt1-dev lib32z1-dev qt5-default libqt5webkit5-dev build-essential python-lxml python-pip xvfb python-dev libmysqlclient-dev
+sudo apt-get install libxml2-dev libxslt1-dev lib32z1-dev qt5-default libqt5webkit5-dev build-essential python-lxml python-pip xvfb python-dev libmysqlclient-dev mysql-client-core-5.6
 sudo apt-get install python-pip python-setuptools
 pip install -r requirements.txt
