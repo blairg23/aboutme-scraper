0e708a939396592ae95baa174d2be7dffb6c7e95
parvusi <hutchpersons@gmail.com>
2016-01-21 02:57:22 +0700

    Update reset.py

--- reset.py
+++ reset.py
@@ -1,10 +1,5 @@
 import settings
 
-#WARNING: This will reset all logs. 
-#The already scraped files will still be retained in the user_data folder.
-#You will have to manually delete those files.
-
 open('%s%s' % (settings.USER_LIST_DIR, settings.USER_LIST_LOG), 'w').close()
 open('%s%s' % (settings.COMPLETED_PATH, settings.COMPLETED_SEARCH_TERMS), 'w').close()
 open('%s%s' % (settings.COMPLETED_PATH, settings.COMPLETED_USER_LIST), 'w').close()
-open('%s%s' % (settings.USER_LIST_DIR, settings.USER_DATA_POPULATED), 'w').close()
